c6279817db9b88bb7bd11085ba57b2716b8529bb
AdvellerdC <84400051+AdvellerdC@users.noreply.github.com>
2021-10-20 20:17:30 +0400

    a

--- Project/src/App.java
+++ Project/src/App.java
@@ -9,6 +9,7 @@ import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JTextField;
+import javax.swing.plaf.basic.BasicEditorPaneUI;
 import javax.swing.text.JTextComponent;
 
 public class App {
@@ -21,23 +22,23 @@ public class App {
         jFrame.setLayout(null);
         jFrame.setResizable(false);
         // combobox
-        String choices[] = { "Base64", "epoch" };
+        String choices[] = { "Base64/encode", "ascii", "Base64/decode" };
         JComboBox comboBox = new JComboBox(choices);
-        comboBox.setBounds(210, 90, 45, 25);
+        comboBox.setBounds(190, 90, 70, 25);
         comboBox.setVisible(true);
         // textbox first one
         JTextField textField = new JTextField();
-        textField.setBounds(135, 90, 70, 25);
+        textField.setBounds(120, 90, 70, 25);
         textField.setVisible(true);
         // textbox sec one
         JTextField jTextField = new JTextField();
-        jTextField.setBounds(140, 190, 120, 50);
+        jTextField.setBounds(155, 160, 70, 25);
         jTextField.setVisible(true);
         // button
         JButton jButton = new JButton();
         jButton.setBackground(Color.GRAY);
         jButton.setVisible(true);
-        jButton.setBounds(145, 120, 80, 30);
+        jButton.setBounds(150, 120, 80, 30);
         jButton.setText("Convert");
         jButton.addActionListener(new ActionListener() {
             @Override
@@ -46,13 +47,34 @@ public class App {
                     JOptionPane.showMessageDialog(jFrame, "You must enter something into the textbox",
                             "An error has occured", JOptionPane.WARNING_MESSAGE);
                 } else if (textField.getText().length() != 0) {
-                    if (comboBox.getSelectedItem() == "Base64") {
+                    if (comboBox.getSelectedItem() == "Base64/encode") {
 
-                        byte[] bytes = textField.toString().getBytes("UTF-8");
-                        String encoded = Base64.getEncoder().encodeToString(bytes);
+                        String encoded = Base64.getEncoder().encodeToString(textField.getText().getBytes());
                         jTextField.setText(encoded);
 
                     }
+                    if (comboBox.getSelectedItem() == "Base64/decode") {
+                        try {
+                        byte[] decodedBytes = Base64.getDecoder().decode(textField.getText().getBytes());
+                        String s = new String(decodedBytes);
+                        jTextField.setText(s);
+                        }
+                        catch(Exception e) {
+                            JOptionPane.showMessageDialog(jFrame, "Could not find character for the input", "An erron has occured", JOptionPane.INFORMATION_MESSAGE);
+                        }
+
+                    }
+                    if (comboBox.getSelectedItem() == "ascii") {
+                        try {
+                        int asciiVal = Integer.parseInt(textField.getText());
+                        String str = new Character((char)asciiVal).toString();
+                        jTextField.setText(str);
+                        }
+                        catch (Exception e) {
+                            
+                        }
+                    }
+
                 }
             }
         });
