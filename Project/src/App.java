3891a1c41bdf711a9e415acb0640a242b83cda45
AdvellerdC <84400051+AdvellerdC@users.noreply.github.com>
2021-10-20 19:12:25 +0400

    .

--- Project/src/App.java
+++ Project/src/App.java
@@ -1,5 +1,7 @@
 import java.awt.Color;
 import java.awt.event.*;
+import java.util.Base64;
+
 import javax.swing.Action;
 import javax.swing.JButton;
 import javax.swing.JComboBox;
@@ -9,41 +11,48 @@ import javax.swing.JOptionPane;
 import javax.swing.JTextField;
 import javax.swing.text.JTextComponent;
 
-
-
 public class App {
-    
+
     public static void main(String[] args) throws Exception {
-        
+
         JFrame jFrame = new JFrame();
-        jFrame.setSize(400,300);
+        jFrame.setSize(400, 300);
         jFrame.setVisible(true);
         jFrame.setLayout(null);
         jFrame.setResizable(false);
-        //combobox
-        String choices[]={"ascii", "epoch"};        
-        JComboBox comboBox = new JComboBox(choices); 
-        comboBox.setBounds(210,90,45,25);
+        // combobox
+        String choices[] = { "Base64", "epoch" };
+        JComboBox comboBox = new JComboBox(choices);
+        comboBox.setBounds(210, 90, 45, 25);
         comboBox.setVisible(true);
-        //textbox fist one
+        // textbox first one
         JTextField textField = new JTextField();
-        textField.setBounds(135,90,70,25);
+        textField.setBounds(135, 90, 70, 25);
         textField.setVisible(true);
-        //textbox sec one
+        // textbox sec one
         JTextField jTextField = new JTextField();
-        jTextField.setBounds(140, 160, 90,30);
+        jTextField.setBounds(140, 190, 120, 50);
         jTextField.setVisible(true);
-        //button
+        // button
         JButton jButton = new JButton();
         jButton.setBackground(Color.GRAY);
         jButton.setVisible(true);
-        jButton.setBounds(145,120,80,30);
+        jButton.setBounds(145, 120, 80, 30);
         jButton.setText("Convert");
-        jButton.addActionListener(new ActionListener(){ 
+        jButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(java.awt.event.ActionEvent ev) {
                 if (textField.getText().length() == 0) {
-                    JOptionPane.showMessageDialog(jFrame, "You must enter something into the textbox", "An error has occured", JOptionPane.WARNING_MESSAGE);
+                    JOptionPane.showMessageDialog(jFrame, "You must enter something into the textbox",
+                            "An error has occured", JOptionPane.WARNING_MESSAGE);
+                } else if (textField.getText().length() != 0) {
+                    if (comboBox.getSelectedItem() == "Base64") {
+
+                        byte[] bytes = textField.toString().getBytes("UTF-8");
+                        String encoded = Base64.getEncoder().encodeToString(bytes);
+                        jTextField.setText(encoded);
+
+                    }
                 }
             }
         });
@@ -53,5 +62,5 @@ public class App {
         jFrame.add(comboBox);
         jFrame.add(jTextField);
     }
-    
+
 }
